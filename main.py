f89416f225ebdd9e083ea5e890be0ea3488d438f
VaroonSB <varoonsb1311@gmail.com>
2025-08-30 16:27:56 +0530

    rag chain

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+
 
 load_dotenv()
 
+
 def main():
     print("Hello from medium-blog-rag!")
+
+    llm = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct", temperature=0)
+
+    query = "What is a vector data base?"
+    chain = PromptTemplate.from_template(template=query) | llm
+    result = chain.invoke(input={})
+    print(result.content)
+
+    vector_store = PineconeVectorStore(
+        embedding=HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        ),
+        index_name="medium-blog-embeddings-index",
+    )
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+
+    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+
+    retrieval_chain = create_retrieval_chain(
+        retriever=vector_store.as_retriever(), combine_docs_chain=combine_docs_chain
+    )
+
+    result = retrieval_chain.invoke(input={"input": query})
+
+    print("Result from RAG:\n")
+    print(result)
 
 
 if __name__ == "__main__":
